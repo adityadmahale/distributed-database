ee70f4d490a5f4dfe4d96162940b6d2800740485
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-04-07 11:02:35 -0300

    Added Utils & Modified Data

---
+++ src/main/java/ca/dal/distributed/dpg1/Controllers/LoggerModule/enums/LoggerType.java
@@ -0,0 +1,21 @@
+package ca.dal.distributed.dpg1.Controllers.LoggerModule.Enums;
+
+import ca.dal.distributed.dpg1.Controllers.LoggerModule.Utils.Constants;
+
+/**
+ * @author Bharatwaaj Shankaranarayanan
+ * @description Logger Type enum for defining which type of logger is required
+ */
+public enum LoggerType {
+    
+    EVENT_LOGGER, QUERY_LOGGER, GENERAL_LOGGER;
+
+    public String getLoggerFile(){
+        switch (this) { 
+            case EVENT_LOGGER: return Constants.LOG_FILE_EVENT; 
+            case QUERY_LOGGER: return Constants.LOG_FILE_QUERY; 
+            case GENERAL_LOGGER: return Constants.LOG_FILE_GENERAL;  
+            default: throw new IllegalStateException(); 
+        } 
+    }
+}
