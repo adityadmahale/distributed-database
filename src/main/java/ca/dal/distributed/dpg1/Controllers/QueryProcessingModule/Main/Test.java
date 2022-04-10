9057e157e5b103f9ee5c21c2456c2160356fc02f
Ankush Mudgal <an564394@dal.ca>
2022-04-10 14:14:41 -0300

    Updates to Dummy Test File - to be deleted later.

--- src/main/java/ca/dal/distributed/dpg1/Controllers/QueryProcessingModule/Main/Test.java
+++ src/main/java/ca/dal/distributed/dpg1/Controllers/QueryProcessingModule/Main/Test.java
@@ -1,23 +1,30 @@
 package ca.dal.distributed.dpg1.Controllers.QueryProcessingModule.Main;
 
+import ca.dal.distributed.dpg1.Controllers.QueryProcessingModule.Utils.MetaDataHandler;
 import ca.dal.distributed.dpg1.Controllers.TransactionModule.Exceptions.TransactionExceptions;
-import com.sun.security.jgss.GSSUtil;
-
-import java.nio.file.Files;
 
 public class Test {
 
     public static void main(String[] args) throws TransactionExceptions {
 
         QueryExecutor executor = new QueryExecutor();
-        //executor.processInputQuery("CREATE DATABASE testDB;");
-        //executor.processInputQuery("drop database testDB;");
-        executor.processInputQuery("use database testDB;");
-        //executor.processInputQuery("CREATE TABLE students (id INT, name TEXT);");
-       //executor.processInputQuery("INSERT INTO students (id, name) VALUES (2, TestName);");
-        System.out.println(executor.processInputQuery("SELECT * FROM students;").toString());
-        //System.out.println(executor.processInputQuery("SELECT DISTINCT id FROM students;"));
-        //executor.processInputQuery("TRUNCATE TABLE students;");
+        //executor.processInputQuery("CREATE DATABASE localDBTest;");
+        //executor.processInputQuery("drop database localDBTest;");
+       // executor.processInputQuery("use database localDBTest;");
+        //executor.processInputQuery("drop table TestTable;");
+       //executor.processInputQuery("CREATE TABLE TestTable (id INT PRIMARY KEY, name TEXT REFERENCES TEST2(id));");
+       /*executor.processInputQuery("INSERT INTO TestTable (id, name) VALUES (2, TestName);");
+       System.out.println(executor.processInputQuery("SELECT * FROM TestTable;").toString());
+       System.out.println(executor.processInputQuery("SELECT DISTINCT id FROM TestTable;"));*/
+        //executor.processInputQuery("TRUNCATE TABLE TestTable;");
+
+        //Test Commit
+     /*   executor.processInputQuery("Start Transaction;");
+        executor.processInputQuery("INSERT INTO TestTable (id, name) VALUES (9, TestTransaction);");
+        executor.processInputQuery("rollback;");*/
+        //executor.processInputQuery("commit;");
+
+
 
     }
 }
